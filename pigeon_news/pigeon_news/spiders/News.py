f0ab12459fe079324bc5fffc41a204c881636366
Jean Santos <jsantos@igspo.ogmaster.local>
2020-07-20 22:09:35 -0300

    first parse and add pycache to .gitignore

---
+++ pigeon_news/pigeon_news/spiders/News.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'News'
+    allowed_domains = ['valorinveste.globo.com']
+    start_urls = ['http://valorinveste.globo.com/rss/valorinveste/ultimas-noticias']
+
+    def parse(self, response):
+        for article in response.css("item"):
+            title = article.css("title").extract_first()
+            yield{'title': title}
