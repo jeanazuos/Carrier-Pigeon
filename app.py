c47e1bf138e26b017e69efe2c59bab8e50f91229
Jean Santos <jsantos@igspo.ogmaster.local>
2020-08-21 17:48:27 -0300

    import logging and add info log

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from pigeon_news.spiders.News import NewsSpider
+from datetime import datetime
 import schedule
 import time
 import os
+import logging
 
 # Job to be run every x minutes
 def job():
+    logging.info("Inicio do job { datetime.now().strftime('%m/%d/%Y, %H:%M:%S') }")
     process = CrawlerProcess(get_project_settings())
     process.crawl(NewsSpider)
     process.start()
